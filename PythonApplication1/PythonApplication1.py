7a81c270b6a07ffc879d1427db53bfe136e2d1a2
Tom Kay <tomkay@outlook.com>
2018-08-30 22:10:48 +0100

    First commit

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,17 @@
+import configparser
+from pathlib import Path
 
+def overwrite_check(filepath): #check that file is older than the file replacing it
+    path = Path(filepath)
+    info = path.stat()
+    print(info.st_mtime)
+
+
+config = configparser.ConfigParser()
+config['settings'] = {'backupListLocation': '.\filelist.txt',
+                     'blackList': '.\blacklist.txt',
+                     }
+with open('example.ini', 'w') as configfile:
+    config.write(configfile)
+
+overwrite_check("C:\\")
---
+++ README.md
@@ -0,0 +1,2 @@
+# Python_Auto_Backup
+python program for backing up files to either a directory or the cloud
